e20cbfead86d2f3407c5a1cc5d9b4bfaa8e4b7a7
BlackEndless <127990034+BlackEnd1ess@users.noreply.github.com>
2024-04-14 15:17:36 +0200

    Delete debugger.py

--- debugger.py
+++
@@ -1,9 +0,0 @@
-import player,crate,item,_core,environment,animation,objects,settings,status,ui,level
-from ursina import *
-
-cc=_core
-def information_output():
-	ppos=cc.playerInstance[0]
-	print(f'player_position::{ppos.position}')
-	print(f'is_landed::{ppos.landed}')
-	print(f'is_jumping::{ppos.jumping}')
