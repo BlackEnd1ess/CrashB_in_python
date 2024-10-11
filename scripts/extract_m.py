69e900525e74105929965af6ce42ac8b74e5dc2d
BlackEndless <127990034+BlackEnd1ess@users.noreply.github.com>
2024-10-11 02:50:38 +0200

    Update extract_m.py

--- scripts/extract_m.py
+++ scripts/extract_m.py
@@ -1,9 +1,13 @@
+## This Code works only with CrashEdit Tool. This Tool must run in maximized Window and In order for the tool to work, the first frame or item must be at the top where you can click on it so that each 
+## frame is processed individually. There are several factors to consider: screen resolution and font size/zoom on the desktop. In this case for me it would be 1920x1080 and text size at 100%, it should 
+## work without any problems. While the tool is working you should not move the mouse or press a key, the process ends after the loop is finished.
+
 import pygetwindow as gw
 import time,pyautogui
 
 win=gw.getWindowsWithTitle('CrashEdit')
-NAME='gorilla'
-outf='C:\\Users\-_-\\Desktop\\evp\\'+NAME+'\\frame'
+NAME='FOLDER_NAME'
+outf='D:\\evp\\'+NAME+'\\frame'
 t=time
 cnt=33
 
